140700f0f54e59dc0209b5ab61d9c51c86e4ef4f
Polillo, M. M <matheus_polillo@yahoo.com>
2021-02-10 20:43:43 -0300

    env dot skill

--- dotSkills/index.js
+++ dotSkills/index.js
@@ -1,4 +1,5 @@
 module.exports = {
 	'sch': require('./schedule.js'),
-	'status': () => { return { type: 'reply', message: 'Online' }; }
+	'status': () => { return { type: 'reply', message: 'Online' }; },
+	'env': () => { return { type: 'reply', message: process.env.ENVINROMENT } }
 };
